8eed39430bbf69ee2aa0ab788c57a024aa28f47a
Ronak1500 <136429788+Ronak1500@users.noreply.github.com>
2023-06-14 11:27:03 +0530

    Update Syllabus.js

--- backend/models/Syllabus.js
+++ backend/models/Syllabus.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const SyllabusSchema = mongoose.Schema({
    syllabus:Array,
-
+   status:true,
 })
 
 module.exports = mongoose.model("Syllabus", SyllabusSchema);
